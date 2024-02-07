01a97b2979aca481a3c46fcde4f4e8db5c53461f
NikiBober <95879723+NikiBober@users.noreply.github.com>
2024-02-07 13:15:17 +0200

    0.4 Game Unlock system
    Complete mission system (generation and claim reward);
    games unlock system depends on amount of premium points;
    points and unlocked games is saved between sessions;
    most of UI.
    Also Audio manager and loading screen.

--- Assets/Scripts/GameUnlocker.cs
+++ Assets/Scripts/GameUnlocker.cs
@@ -1,16 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class GameUnlocker : MonoBehaviour
+public class GameUnlocker : Singleton<GameUnlocker>
 {
     [SerializeField] private GameObject[] _gameButtons;
     [SerializeField] private GameObject _lockedImage;
+    [SerializeField] private int[] _unlockValues;
+    [SerializeField] private TextMeshProUGUI _unlockValueText;
+
+    private void OnEnable()
+    {
+        EventManager.OnPremiumScoreUpdate += TryUnlock;
+    }
 
     private void Start()
     {
-        _lockedImage.SetActive(true);
-        var position = _gameButtons[1].transform.position;
-        _lockedImage.transform.position = position;
+        for (int i = 0; i < SaveData.UnlockedGamesCount; i++)
+        {
+            _gameButtons[i].SetActive(true);
+        }
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        if (SaveData.UnlockedGamesCount < _gameButtons.Length)
+        {
+            _gameButtons[SaveData.UnlockedGamesCount].SetActive(true);
+            _lockedImage.transform.position = _gameButtons[SaveData.UnlockedGamesCount].transform.position;
+            _unlockValueText.text = _unlockValues[SaveData.UnlockedGamesCount].ToString();
+        }
+        else
+        {
+            _lockedImage.SetActive(false);
+            EventManager.OnPremiumScoreUpdate -= TryUnlock;
+        }
+    }
+
+    private void TryUnlock(int score)
+    {
+        if (SaveData.PremiumScore > _unlockValues[SaveData.UnlockedGamesCount])
+        {
+            SaveData.UnlockNextGame();
+            Refresh();
+        }
     }
 }
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -8,6 +8,7 @@ public class SaveData : Singleton<SaveData>
 {
     private static readonly string _coinsScoreLabel = "CoinsScore";
     private static readonly string _premiumScoreLabel = "PremiumScore";
+    private static readonly string _unlockedGamesCountLabel = "UnlockedGamesCount";
 
     private static int _coinsScore = 0;
     public static int CoinsScore { get => _coinsScore; }
@@ -15,18 +16,30 @@ public class SaveData : Singleton<SaveData>
     private static int _premiumScore = 0;
     public static int PremiumScore { get => _premiumScore; }
 
+    private static int _unlockedGamesCount = 1;
+    public static int UnlockedGamesCount { get => _unlockedGamesCount; }
+
     private void OnEnable()
     {
         //EventManager.OnGameOver += GameOverHandler;
         EventManager.OnPremiumScoreUpdate += UpdatePremiumScoreHandler;
+        //PlayerPrefs.SetInt(_unlockedGamesCountLabel, 1);
+        //PlayerPrefs.SetInt(_premiumScoreLabel, 1);
 
         _coinsScore = PlayerPrefs.GetInt(_coinsScoreLabel);
         _premiumScore = PlayerPrefs.GetInt(_premiumScoreLabel);
+        _unlockedGamesCount = PlayerPrefs.GetInt(_unlockedGamesCountLabel, _unlockedGamesCount);
     }
 
-    public static bool IsFirstLaunch()
+    //public static bool IsFirstLaunch()
+    //{
+    //    return !PlayerPrefs.HasKey(_coinsScoreLabel);
+    //}
+
+    public static void UnlockNextGame()
     {
-        return !PlayerPrefs.HasKey(_coinsScoreLabel);
+        _unlockedGamesCount++;
+        PlayerPrefs.SetInt(_unlockedGamesCountLabel, _unlockedGamesCount);
     }
 
     private void UpdateCoinsScoreHandler(int amount)
